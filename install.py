cee706c8814f36ca3f0889bf27f414b68ccd3edb
Bruno Madeira <bmad.works@gmail.com>
2024-09-05 21:40:52 +0100

    attempt to install pyastar2d on first installation

--- install.py
+++ install.py
@@ -8,3 +8,7 @@ if __name__ == "__main__":
     script_path = os.path.abspath(__file__)
     project_dir = os.path.dirname(script_path)
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", f"{project_dir}"])
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "pyastar2d==1.0.6"])
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to install optional package pyastar2d due to the following error:\n{e}")
