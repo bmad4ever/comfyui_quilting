39b4b2c9585eae1a4058415f0edc46b5f47151e4
Bruno Madeira <bmad.works@gmail.com>
2024-09-05 21:15:50 +0100

    update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -1,11 +1,14 @@
 import subprocess
 import sys
 
+
 def uninstall_package(package_name):
     subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', '-y', package_name])
 
+
 if __name__ == "__main__":
-    packages_to_uninstall = ['bmq_comfyui_utils', 'bmquilting']
+    # note: last item cleans dist-info, if exists due to installation via .toml
+    packages_to_uninstall = ['bmq_comfyui_utils', 'bmquilting', 'comfyui_quilting']
     for package in packages_to_uninstall:
         print(f"Uninstalling {package}...")
         uninstall_package(package)
